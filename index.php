82c8e5066a6b8ebd0f5fba24bd58c841ccb6e24b
André Moura <andre.benedicto@etec.sp.gov.br>
2020-04-18 22:49:33 -0300

    Atualizando versão do Bootstrap para o 4

--- index.php
+++ index.php
@@ -1,83 +1,82 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="utf-8">
 
-		<title>Twitter clone</title>
+		<title>Twitter Clone</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- jquery - link cdn -->
-		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 		<!-- bootstrap - link cdn -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	
 		<script>
-			// código javascript						
+			// código javascript            
 		</script>
 	</head>
-
 	<body>
 
 		<!-- Static navbar -->
-	    <nav class="navbar navbar-default navbar-static-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <img src="imagens/icone_twitter.png" />
-	        </div>
-	        
-	        <div id="navbar" class="navbar-collapse collapse">
-	          <ul class="nav navbar-nav navbar-right">
-	            <li><a href="inscrevase.php">Inscrever-se</a></li>
-	            <li class="">
-	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
-					<ul class="dropdown-menu" aria-labelledby="entrar">
-						<div class="col-md-12">
-				    		<p>Você possui uma conta?</h3>
-				    		<br />
-							<form method="post" action="" id="formLogin">
-								<div class="form-group">
-									<input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Usuário" />
+		<nav class="navbar navbar-expand-md navbar-light bg-light border-bottom mb-4 p-0">
+			<div class="container">
+				<img class="navbar-brand" src="./imagens/icone_twitter.png">
+
+				<button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="navbar-nav ml-auto mt-2 mt-md-0">
+						<li class="nav-item">
+							<a class="nav-link" href="inscrevase.php">Inscrever-se</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+								Entrar
+							</a>
+							
+							<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="entrar">
+								<div class="col-md-12 mb-3">
+									<p>Você possui uma conta?</p>
+
+									<form method="post" action="" id="formLogin">
+										<div class="form-group">
+											<input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Usuário">
+										</div>
+						
+										<div class="form-group">
+											<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha">
+										</div>
+						
+										<button type="buttom" class="btn btn-primary" id="btn_login">
+											Entrar
+										</button>
+						
+									</form>
 								</div>
-								
-								<div class="form-group">
-									<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
-								</div>
-								
-								<button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
+							</ul>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
 
-								<br /><br />
-								
-							</form>
-						</form>
-				  	</ul>
-	            </li>
-	          </ul>
-	        </div><!--/.nav-collapse -->
-	      </div>
-	    </nav>
+		<div class="container">
 
+			<!-- Main component for a primary marketing message or call to action -->
+			<div class="jumbotron">
+				<h1 class="display-4 font-weight-bold">Bem vindo ao twitter clone</h1>
+				<p>Veja o que está acontecendo agora...</p>
+			</div>
 
-	    <div class="container">
-
-	      <!-- Main component for a primary marketing message or call to action -->
-	      <div class="jumbotron">
-	        <h1>Bem vindo ao twitter clone</h1>
-	        <p>Veja o que está acontecendo agora...</p>
-	      </div>
-
-	      <div class="clearfix"></div>
+			<div class="clearfix"></div>
 		</div>
 
-
-	    </div>
-	
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	
 	</body>
 </html>
